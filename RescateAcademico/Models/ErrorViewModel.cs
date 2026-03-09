81977b503e9f47d04a839e991857b56c8e4d9e9c
Xerea <Xerea@DESKTOP-NMOT12D>
2026-03-09 17:23:25 -0600

    Add project files.

---
+++ RescateAcademico/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RescateAcademico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
