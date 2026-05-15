2b72c6a7059e394058ae46b3737d712b2f4998fc
Xerea <128547167+Xerea@users.noreply.github.com>
2026-05-15 02:24:39 -0600

    Fix register password validation to match Identity policy

--- RescateAcademico/Controllers/AccountController.cs
+++ RescateAcademico/Controllers/AccountController.cs
@@ -379,7 +379,7 @@ namespace RescateAcademico.Controllers
         public string Email { get; set; } = string.Empty;
 
         [Required]
-        [StringLength(100, MinimumLength = 6)]
+        [StringLength(100, MinimumLength = 8)]
         public string Password { get; set; } = string.Empty;
 
         [Compare("Password")]
